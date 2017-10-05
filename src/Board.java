def83d3106e66aa9c88e1e5d2f93a93ef564ca34
Mark Bojesen <bojesenmark@gmail.com>
2017-10-05 15:22:24 +0100

    initial commit

---
+++ src/Board.java
@@ -0,0 +1,75 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements ActionListener {
+
+    private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection =  false;
+    private boolean downDirection = false;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Image ball;
+    private Image apple;
+    private Image head;
+
+    public Board() {
+
+        addKeyListener(new TAdapter());
+        setBackground(Color.orange);
+        setFocusable(true);
+
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        loadImage();
+        initGame();
+    }
+
+    private void loadImages() {
+        ImageIcon iid = new ImageIcon("dot.png");
+        ball = iid.getImage();
+
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+
+        ImageIcon iih = new ImageIcon("head.png");
+        head = iih.getImage();
+    }
+
+    private void initGame() {
+        dots = 3;
+
+        for (int z = 0; z < dots; z++) {
+            x[z] = 50 - z * 10;
+            y[x] = 50;
+        }
+    }
+
+}
